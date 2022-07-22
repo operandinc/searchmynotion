dc55e4a05dcb96e48af428821b8362088f290e3e
Donwauchopex <donwauchopex@gmail.com>
2022-07-22 12:59:11 -0400

    security fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,24 @@
 /** @type {import('next').NextConfig} */
+
+const { createSecureHeaders } = require("next-secure-headers");
+
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: createSecureHeaders({
+          frameGuard: "deny",
+          contentSecurityPolicy: {
+            directives: {
+              frameAncestors: "none",
+            },
+          },
+        }),
+      },
+    ];
+  },
+
   reactStrictMode: true,
   swcMinify: true,
 };
